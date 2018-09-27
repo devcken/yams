d3872e106690014ae41e20184c22f3caa3f1985a
devcken <devcken@gmail.com>
2018-09-27 16:22:32 +0900

    replace the test lexer with BlockMappingTestLexer

--- src/test/scala/yams/lexers/IndentationSpacesLexerTest.scala
+++ src/test/scala/yams/lexers/IndentationSpacesLexerTest.scala
@@ -7,28 +7,63 @@ package yams.lexers
   * @see [[http://yaml.org/spec/1.2/spec.html#id2777534]]
   */
 class IndentationSpacesLexerTest extends yams.helper.YamsSpec {
-  import yams.YamlLoadError
+  import yams.{BlockIn, YamlLoadError}
+  import yams.tokens.{FlowNodeToken, FlowMappingToken, FlowEntryToken, ScalarToken}
   
-  object IndentationSpacesTestLexer extends IndentationSpacesLexer {
-    def apply(x: String, n: Int): Either[YamlLoadError, Int] =
-      parse(indent(n), x) match {
-        case Success(y, _)      => Right(y)
-        case NoSuccess(m, rest) => Left(YamlLoadError(rest.pos, m))
+  object BlockMappingTestLexer extends BlockStylesLexer {
+    def apply(x: String): Either[YamlLoadError, FlowNodeToken] =
+      parseAll(blockNode(-1, BlockIn), x) match {
+        case NoSuccess(msg, rest) => Left(YamlLoadError(rest.pos, msg))
+        case Success(y, _) => Right(y)
       }
   }
 
-  "Indentation Spaces" in {
-    val xs = List(
-      (" By one space:", 1),
-      ("    By four", 4),
-      ("      spaces", 4)
+  "ex 6.1. Indentation Spaces" in {
+    import yams.tokens.{FlowSequenceToken, Literal}
+    
+    /*
+       Not indented:
+        By one space: |
+           By four
+             spaces
+        Flow style: [    # Leading spaces
+          By two,        # in flow style
+         Also by two,    # are neither
+         	Still by two   # content nor
+           ]             # indentation.
+     */
+    
+    val x = readLines("example/ch6_basic-structures/ex6.1_indentation-spaces.yml", 3 to 11)
+    
+    val expected = Right(
+      FlowMappingToken(
+        List(
+          FlowEntryToken(
+            ScalarToken("Not indented"), 
+            FlowMappingToken(
+              List(
+                FlowEntryToken(
+                  ScalarToken("By one space"),
+                  ScalarToken("By four\\n  spaces\\n", Literal)
+                ),
+                FlowEntryToken(
+                  ScalarToken("Flow style"),
+                  FlowSequenceToken(
+                    List(
+                      ScalarToken("By two"),
+                      ScalarToken("Also by two"),
+                      ScalarToken("Still by two")
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
     )
-
-    for (x <- xs) {
-      val expected = Right(x._2)
-      val actual = IndentationSpacesTestLexer(x._1, x._2)
-
-      assert(expected == actual)
-    }
+    val actual = BlockMappingTestLexer(x)
+    
+    assertResult(expected)(actual)
   }
 }
